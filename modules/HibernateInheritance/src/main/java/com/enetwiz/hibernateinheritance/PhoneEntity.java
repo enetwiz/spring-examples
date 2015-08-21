e5dc2c52342c5c8abd666c708e38e3437462fa0e
Mariusz G <redtrader1@gmail.com>
2015-08-21 17:00:30 +0200

    Module > HibernateInheritance > PhoneEntity() - removed not required strategy from @GeneratedValue annotation

--- modules/HibernateInheritance/src/main/java/com/enetwiz/hibernateinheritance/PhoneEntity.java
+++ modules/HibernateInheritance/src/main/java/com/enetwiz/hibernateinheritance/PhoneEntity.java
@@ -4,7 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
@@ -21,7 +20,7 @@ import javax.persistence.Table;
 public abstract class PhoneEntity {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue
     private int id = 0;
     
     @Column
